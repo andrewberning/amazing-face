8d2b42c88e297c843e3a13af062bb7d72f14440e
andrewberning <berningan89@gmail.com>
2023-04-27 20:48:13 -0700

    modified App.js

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends Component {
   }
   onSubmitSignIn = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://amazing-face-api.onrender.com/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -74,7 +74,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://amazing-face-api.onrender.comst:/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
